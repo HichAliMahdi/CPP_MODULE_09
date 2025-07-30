aad703629362a6fe8b06e71bcb5ecae1c5e75ddc
Hichem Abderrahmane Ali Mahdi <hali-mah@2-G-5.42heilbronn.de>
2025-07-30 16:10:19 +0200

    RPN.cpp finished, next step do some tests and see for errors and bugs

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -42,3 +42,39 @@ double RPN::performOperation(double a, double b, const std::string& op) const {
     }
 }
 
+void RPN::processToken(const std::string& token) {
+    if (isValidNumber(token)) {
+        double number = token[0] - '0';
+        _stack.push(number);
+    }
+    else if (isOperator(token)) {
+        if (_stack.size() < 2) {
+            throw std::runtime_error("Error: insufficient operands");
+        }
+        double b = _stack.top(); _stack.pop();
+        double a = _stack.top(); _stack.pop();
+        double result = performOperation(a, b, token);
+        _stack.push(result);
+    }
+    else {
+        throw std::runtime_error("Error: invalid token '" + token + "'");
+    }
+}
+
+double RPN::calculate(const std::string& expression) {
+    while (!_stack.empty()) {
+        _stack.pop();
+    }
+    std::istringstream iss(expression);
+    std::string token;
+    while (iss >> token) {
+        processToken(token);
+    }
+    if (_stack.empty()) {
+        throw std::runtime_error("Error: empty expression");
+    }
+    if (_stack.size() != 1) {
+        throw std::runtime_error("Error: invalid expression - too many operands");
+    }
+    return _stack.top();
+}
