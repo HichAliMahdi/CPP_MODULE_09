b01c3f6d73ea764605d5c90f28fda7eb3a510b2e
Hichem Abderrahmane Ali Mahdi <hali-mah@2-G-5.42heilbronn.de>
2025-07-30 16:07:09 +0200

    handling some edge cases and continuing with methods

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -10,3 +10,35 @@ RPN& RPN::operator=(const RPN& other) {
 }
 RPN::~RPN(){}
 
+bool RPN::isOperator(const std::string& token) const {
+    return (token == "+" || token == "-" || token == "*" || token == "/");
+}
+
+bool RPN::isValidNumber(const std::string& token) const {
+    if (token.length() != 1) {
+        return false;
+    }
+    return (token[0] >= '0' && token[0] <= '9');
+}
+
+double RPN::performOperation(double a, double b, const std::string& op) const {
+    if (op == "+") {
+        return a + b;
+    }
+    else if (op == "-") {
+        return a - b;
+    }
+    else if (op == "*") {
+        return a * b;
+    }
+    else if (op == "/") {
+        if (b == 0) {
+            throw std::runtime_error("Error: division by zero");
+        }
+        return a / b;
+    }
+    else {
+        throw std::runtime_error("Error: unknown operator");
+    }
+}
+
