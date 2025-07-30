2cfc664ce68bcb981ec4efafc5f08a3819faa1d7
Hichem Abderrahmane Ali Mahdi <hali-mah@2-G-5.42heilbronn.de>
2025-07-30 16:04:52 +0200

    Simple formalities for ex01 orthodoxal form

---
+++ ex01/RPN.cpp
@@ -0,0 +1,12 @@
+#include "RPN.hpp"
+
+RPN::RPN(){}
+RPN::RPN(const RPN& other) : _stack(other._stack){}
+RPN& RPN::operator=(const RPN& other) {
+    if (this != &other) {
+        _stack = other._stack;
+    }
+    return *this;
+}
+RPN::~RPN(){}
+
