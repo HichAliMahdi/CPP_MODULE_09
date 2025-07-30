91ca8396c6210343ad0461bd49ac0c0a03beda0c
Hichem Abderrahmane Ali Mahdi <hali-mah@1-E-31.42heilbronn.de>
2025-07-30 19:54:38 +0200

    Found issues with my code: correction in progress

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -5,29 +5,27 @@
 int main(int argc, char* argv[]){
     try{
         PmergeMe processor;
-
         processor.processArguments(argc, argv);
-
         processor.showInitialState();
 
-        PmergeMe firstProcessor = processor;
-        double beginTime = PmergeMe::getTimestamp();
-        firstProcessor.executePrimarySort();
-        double primaryDuration = PmergeMe::getTimestamp() - beginTime;
+        PmergeMe vectorProcessor = processor;
+        double startTime = PmergeMe::getTimestamp();
+        vectorProcessor.executeVectorSort();
+        double vectorTime = PmergeMe::getTimestamp() - startTime;
 
-        PmergeMe secondProcessor = processor;
-        beginTime = PmergeMe::getTimestamp();
-        secondProcessor.executeSecondarySort();
-        double secondaryDuration = PmergeMe::getTimestamp() - beginTime;
+        PmergeMe dequeProcessor = processor;
+        startTime = PmergeMe::getTimestamp();
+        dequeProcessor.executeDequeSort();
+        double dequeTime = PmergeMe::getTimestamp() - startTime;
 
-        firstProcessor.showFinalState();
+        vectorProcessor.showFinalState();
 
-        int elementCount = argc - 1;
+        size_t elementCount = vectorProcessor.getSize();
         std::cout << std::fixed << std::setprecision(5);
         std::cout << "Time to process a range of " << elementCount 
-                  << " elements with std::vector : " << primaryDuration << " us" << std::endl;
+                  << " elements with std::vector : " << vectorTime << " us" << std::endl;
         std::cout << "Time to process a range of " << elementCount 
-                  << " elements with std::deque : " << secondaryDuration << " us" << std::endl;
+                  << " elements with std::deque : " << dequeTime << " us" << std::endl;
         
     } catch (const std::exception& error) {
         std::cerr << error.what() << std::endl;
